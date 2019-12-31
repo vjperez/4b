94df16759691470ef926049185d4312eae10c548
victor j. perez <vijapefi@gmail.com>
2019-12-31 17:23:47 -0400

    portada

--- escritos/js/portada.js
+++ escritos/js/portada.js
@@ -43,33 +43,33 @@ function showArray(){
 function putHtml(elHtml, datos){  
   var q = 0;     var ePrintIndex = 0;
   while(q < datos.length){
-   if(Array.isArray(datos[q])){ //if it is not an array, it does not have entries ... dont print 
-    var e = 0;
-    while(e < datos[q].length){
-      elHtml[ePrintIndex%showing] += "<div class='entry'>";
-      elHtml[ePrintIndex%showing] += "<div class='hora'>" + datos[q][e][1] + "<span class='entry-post-text'>" + datos[q][e][0] + "</span></div>";
-      if(datos[q][e][9] == 0 || datos[q][e][10] == 0 || datos[q][e][8] == "") {
-      	//picture with ancho 0 or alto 0 or  name is "" : meaning there is no picture for this entry
-      }else{
-        //elHtml[index%showing] += "<a href='escritos/php/sacalo/muestralo.php?entry=" + entry[index][8].substring(0, entry[index][8].indexOf(".")) + "'><img src='loaded/" + entry[index][8] +  "' alt='foto' class='entry-main-img'></a>";
-        elHtml[ePrintIndex%showing] += "<img src='loaded/" + datos[q][e][8] +  "' alt='baloncesto beisbol volibol futbol soccer infantil juvenil puerto rico' class='entry-main-img'>";
+      if(Array.isArray(datos[q])){ //if it is not an array, it does not have entries ... dont print 
+        var e = 0;
+        while(e < datos[q].length){
+          elHtml[ePrintIndex%showing] += "<div class='entry'>";
+          elHtml[ePrintIndex%showing] += "<div class='hora'>" + datos[q][e][1] + "<span class='entry-post-text'>" + datos[q][e][0] + "</span></div>";
+          if(datos[q][e][9] == 0 || datos[q][e][10] == 0 || datos[q][e][8] == "") {
+            //picture with ancho 0 or alto 0 or  name is "" : meaning there is no picture for this entry
+          }else{
+            //elHtml[index%showing] += "<a href='escritos/php/sacalo/muestralo.php?entry=" + entry[index][8].substring(0, entry[index][8].indexOf(".")) + "'><img src='loaded/" + entry[index][8] +  "' alt='foto' class='entry-main-img'></a>";
+            elHtml[ePrintIndex%showing] += "<img src='loaded/" + datos[q][e][8] +  "' alt='baloncesto beisbol volibol futbol soccer infantil juvenil puerto rico' class='entry-main-img'>";
+          }
+          elHtml[ePrintIndex%showing] += "<div class='tag12'>";
+          elHtml[ePrintIndex%showing] += "<a href='portada.php?q=" + "2" + getDeporte(datos[q][e][2]) + "2" + getArea(datos[q][e][3]) + "2" + "'><img src='icon/" + datos[q][e][2]  + "' alt='foto' class='entry-sport-img'></a><a href='portada.php?q=" + "4" + getDeporte(datos[q][e][2]) + "4" + getArea(datos[q][e][3]) + "4" + "'><span>" + datos[q][e][3]  + "</span></a>";
+          //if(entry[index][3].indexOf("nternacional") > -1) elHtml[index%showing] += "<img src='icon/pr.png' alt='foto' class='entry-country-img'>";
+          elHtml[ePrintIndex%showing] += "</div>";
+          elHtml[ePrintIndex%showing] += "<div class='tag34'><a href='portada.php?q=" + "8" + getDeporte(datos[q][e][2]) + "8" + getArea(datos[q][e][3]) + "8" + ":" + datos[q][e][4] + explota(datos[q][e][4]) + "'>" + datos[q][e][4] + "</a></div>";
+          elHtml[ePrintIndex%showing] += "<div class='tag34'><a href='portada.php?q=" + "8" + getDeporte(datos[q][e][2]) + "8" + getArea(datos[q][e][3]) + "8" + ":" + datos[q][e][5] + explota(datos[q][e][5]) + "'>" + datos[q][e][5] + "</a></div>";
+          elHtml[ePrintIndex%showing] += "<div class='tag5'> <a href='portada.php?q=" + "8" + getDeporte(datos[q][e][2]) + "8" + getArea(datos[q][e][3]) + "8" + ":" + datos[q][e][6] + explota(datos[q][e][6]) + "'>" + datos[q][e][6] + "</a></div>";
+          if(datos[q][e][7] == ""){
+          }else{
+            elHtml[ePrintIndex%showing] += "<div class='comentario'><p>" + datos[q][e][7] + "</p></div>";
+          }
+          elHtml[ePrintIndex%showing] += "</div>";
+          e++;     ePrintIndex++;
+        }
       }
-      elHtml[ePrintIndex%showing] += "<div class='tag12'>";
-      elHtml[ePrintIndex%showing] += "<a href='portada.php?q=" + "2" + getDeporte(datos[q][e][2]) + "2" + getArea(datos[q][e][3]) + "2" + "'><img src='icon/" + datos[q][e][2]  + "' alt='foto' class='entry-sport-img'></a><a href='portada.php?q=" + "4" + getDeporte(datos[q][e][2]) + "4" + getArea(datos[q][e][3]) + "4" + "'><span>" + datos[q][e][3]  + "</span></a>";
-      //if(entry[index][3].indexOf("nternacional") > -1) elHtml[index%showing] += "<img src='icon/pr.png' alt='foto' class='entry-country-img'>";
-      elHtml[ePrintIndex%showing] += "</div>";
-      elHtml[ePrintIndex%showing] += "<div class='tag34'><a href='portada.php?q=" + "8" + getDeporte(datos[q][e][2]) + "8" + getArea(datos[q][e][3]) + "8" + ":" + datos[q][e][4] + explota(datos[q][e][4]) + "'>" + datos[q][e][4] + "</a></div>";
-      elHtml[ePrintIndex%showing] += "<div class='tag34'><a href='portada.php?q=" + "8" + getDeporte(datos[q][e][2]) + "8" + getArea(datos[q][e][3]) + "8" + ":" + datos[q][e][5] + explota(datos[q][e][5]) + "'>" + datos[q][e][5] + "</a></div>";
-      elHtml[ePrintIndex%showing] += "<div class='tag5'> <a href='portada.php?q=" + "8" + getDeporte(datos[q][e][2]) + "8" + getArea(datos[q][e][3]) + "8" + ":" + datos[q][e][6] + explota(datos[q][e][6]) + "'>" + datos[q][e][6] + "</a></div>";
-      if(datos[q][e][7] == ""){
-      }else{
-        elHtml[ePrintIndex%showing] += "<div class='comentario'><p>" + datos[q][e][7] + "</p></div>";
-      }
-      elHtml[ePrintIndex%showing] += "</div>";
-      e++;     ePrintIndex++;
-    }
-   }
-    q++;	       
+      q++;	       
   }
 }     
 
