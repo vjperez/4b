aa86389316454555530b9912f3da3729f090b33f
vjperez <vijapefi@gmail.com>
2020-11-29 19:41:12 -0400

    bk otro

--- bk/nepeype.sql
+++ bk/nepeype.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 11.0
--- Dumped by pg_dump version 11.0
+-- Dumped from database version 12.4
+-- Dumped by pg_dump version 12.4
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -12,12 +12,30 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+ALTER TABLE ONLY public.bregando DROP CONSTRAINT bregando_nepe_id;
+ALTER TABLE ONLY public.bregando DROP CONSTRAINT bregando_dueno_id_fkey;
+DROP INDEX public.nombre_que_vector_idx;
+DROP INDEX public.donde_vector_idx;
+ALTER TABLE ONLY public.nepe DROP CONSTRAINT nepe_pkey;
+ALTER TABLE ONLY public.dueno DROP CONSTRAINT dueno_username_key;
+ALTER TABLE ONLY public.dueno DROP CONSTRAINT dueno_pkey;
+ALTER TABLE ONLY public.bregando DROP CONSTRAINT bregando_pkey;
+ALTER TABLE public.nepe ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.dueno ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.bregando ALTER COLUMN id DROP DEFAULT;
+DROP SEQUENCE public.nepe_id_seq;
+DROP TABLE public.nepe;
+DROP SEQUENCE public.dueno_dueno_id_seq;
+DROP TABLE public.dueno;
+DROP SEQUENCE public.bregando_bregando_id_seq;
+DROP TABLE public.bregando;
 SET default_tablespace = '';
 
-SET default_with_oids = false;
+SET default_table_access_method = heap;
 
 --
 -- Name: bregando; Type: TABLE; Schema: public; Owner: victordbu
@@ -184,14 +202,14 @@ COPY public.bregando (dueno_id, nepe_id, id) FROM stdin;
 
 COPY public.dueno (id, username, password, last_log, first_log) FROM stdin;
 14	vito	8888	2020-11-27	2019-08-03
-2	lola	2222	2020-11-29	2010-01-01
-1	tito	1111	2020-11-26	2010-01-01
 3	chucho	3333	2020-09-12	2010-01-01
 5	luis	5555	2020-09-12	2010-01-01
 6	cheo	6666	2020-09-12	2010-01-01
 10	krystal	7777	2020-10-10	2019-01-16
 4	papito	4444	2020-10-30	2010-01-01
 18	risi-piston	9999	2020-11-01	2020-11-01
+2	lola	2222	2020-11-29	2010-01-01
+1	tito	1111	2020-11-29	2010-01-01
 \.
 
 
@@ -206,10 +224,10 @@ COPY public.nepe (id, nombre, revisado, media_video_url, media_social_handle, me
 1	barberia de tito	2019-08-19	https://www.youtube.com/embed/rWq_-AexyAo	{"fbk":"","tt":"@tito","igrm":"@tito_en_insta","phn":"787 222 0101"}	{1b.jpg,1a}	{peinador,peluquera,peinadora,"hair professional",estilista,barbero}	{moca,rincon,ponce,loiza}	t	'barber':10 'barberi':1 'estil':9 'hair':7 'peinador':4,6 'peluquer':5 'professional':8 'tit':3	'loiza':4 'moca':1 'ponce':3 'rincon':2	{"lun":"3pm a 8 pm","mar":"","mier":"","jue":"","vier":"todo el diA","sab":"","dom":""}
 4	payasadas de papito el bello	2020-10-03	https://www.youtube.com/embed/AFoFxirvRKU	{"fbk":"papito el payaso","tt":"@papito-payaso","igrm":"@papitoPayaso_en_insta","phn":"787 248 4004"}	{4a.jpeg,4b.jpeg}	{musico,bailarin,orquesta,"entretenimiento actividades",payaso}	{moca,"san german",lares}	t	'activ':10 'bailarin':7 'bell':5 'entreten':9 'music':6 'orquest':8 'papit':3 'payas':1,11	'german':3 'lares':4 'moca':1 'san':2	{"lun":"","mar":"","mier":"","jue":"","vier":"","sab":"3 oct 2020","dom":""}
 3	chucho landscaping	2019-08-03	https://www.youtube.com/embed/0Uk5kZ5k0vY	{"fbk":"chucho de jayuya","tt":"@granchucho","igrm":"","phn":"787 222 0003"}	{3a,3b.jpg}	{"pica grama","corta grama","tumba yerba","limpio patios","trimeo palos","hacemos patios","landscaping de patios"}	{moca,"las marias",mayaguez,caguas}	t	'chuch':1 'cort':5 'gram':4,6 'hac':13 'landscaping':2,15 'limpi':9 'pal':12 'pati':10,14,17 'pic':3 'trime':11 'tumb':7 'yerb':8	'caguas':5 'las':2 'marias':3 'mayaguez':4 'moca':1	{"lun":"","mar":"","mier":"despues de las 12","jue":"","vier":"","sab":"","dom":""}
-11	tutorias lola	2020-11-28	https://www.youtube.com/watch?v=Z3cmddZh6t8	{"fbk":"lolaEnFacebook","tt":"@lolaenTuiter","igrm":"@instagram_lola","phn":"826 4444"}	{11a.jpeg}	{"book reading tutos","leemos tu libros","estudio en grupo"}	{"la universidad","el colegio"}	t	'book':3 'estudi':9 'grup':11 'leem':6 'libr':8 'lol':2 'reading':4 'tut':5 'tutori':1	'colegio':4 'el':3 'la':1 'universidad':2	{"lun":"","mar":"","mier":"","jue":"no llueve hoy","vier":"","sab":"voy sabado","dom":"y voy domingo"}
 34	Taller de Risi	2020-11-01	https://www.youtube.com/watch?v=xOOzmR30_YY	{"fbk":"face de luisa","tt":"","igrm":"","phn":"787 827 2396"}	{34a.jpeg}	{mecanico,"grasa man",transmisiones,"deja danao"}	{"las marias",lares,"san sebastian"}	f	'dana':9 'dej':8 'gras':5 'man':6 'mecan':4 'risi':3 'tall':1 'transmision':7	'lares':3 'las':1 'marias':2 'san':4 'sebastian':5	{"lun":"hasta las 5","mar":"","mier":"","jue":"","vier":"","sab":"cerrado : soy adventista","dom":""}
 29	delivery don vito	2020-10-03	youtu6	{"fbk":"","tt":"@yolollevo","igrm":"","phn":""}	{29a.jpeg,29b.jpeg}	{"yo te llevo la compra"}	{"las marias",maricao,"san sebastian"}	t	'compr':8 'delivery':1 'don':2 'llev':6 'vit':3	'las':1 'marias':2 'maricao':3 'san':4 'sebastian':5	{"lun":"","mar":"","mier":"","jue":"","vier":"","sab":"weekenes","dom":"weekenes"}
 2	lola dona	2020-11-26	https://www.youtube.com/embed/4KjYNuNBOBg	{"fbk":"lola la comelola","tt":"","igrm":"@lola_en_insta","phn":"787 249 0202"}	{2a,2b.jpg}	{repostera,baker,panadera,dulces,postres,"tu bizcochito",pastelillos}	{ponce,coamo,"sabana grande",yauco,guanica}	f	'bak':4 'bizcochit':9 'don':2 'dulc':6 'lol':1 'panader':5 'pastelill':10 'postr':7 'reposter':3	'coamo':2 'grande':4 'guanica':6 'ponce':1 'sabana':3 'yauco':5	{"lun":"","mar":"no te cases","mier":"","jue":"","vier":"","sab":"los weekenes","dom":"los weekenes"}
+11	Tutorias Lola	2020-11-29	https://www.youtube.com/watch?v=mlAGdR1scQs	{"fbk":"lolaEnFacebook","tt":"@lolaenTuiter","igrm":"@instagram_lola","phn":"826 4444"}	{11a.jpeg,11b.jpeg,11c.jpeg}	{"book reading tutos","leemos tu libros","estudio en grupo"}	{"la universidad","el colegio"}	f	'book':3 'estudi':9 'grup':11 'leem':6 'libr':8 'lol':2 'reading':4 'tut':5 'tutori':1	'colegio':4 'el':3 'la':1 'universidad':2	{"lun":"","mar":"","mier":"","jue":"no llueve hoy","vier":"","sab":"voy sabado","dom":"y voy domingo"}
 \.
 
 
